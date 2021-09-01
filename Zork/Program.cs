ba3b9d24fada165f71992fad285e21f7d23f4e0a
xArtoriasx <60054783+xArtoriasx@users.noreply.github.com>
2021-09-01 15:19:51 -0400

    Zork 1.3
    The Game Loop

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,14 +8,43 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, whith a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        outputString = $"You moved {command}.";
+                        break;
+
+                    default:
+                        outputString = "Unrecognized command.";
+                        break;
+
+                }
+
+                Console.WriteLine(outputString);
+            }
         }
 
         private static Commands ToCommand(string commandString)
         {
-            return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+            return Enum.TryParse(commandString, ignoreCase:true, out Commands result) ? result : Commands.UNKNOWN;
         }
     }
 }
